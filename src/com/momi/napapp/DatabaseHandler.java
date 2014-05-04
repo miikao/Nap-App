56e961190089152c3a471d8ea805c237159a5481
Lauri Lode <lauri.lode@gmail.com>
2014-05-04 23:27:50 +0300

    justandroidthings

--- src/com/momi/napapp/DatabaseHandler.java
+++ src/com/momi/napapp/DatabaseHandler.java
@@ -2,62 +2,94 @@ package com.momi.napapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
 	private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "tableSet";
-    
+	private static final String DATABASE_NAME = "tableSet";
+
 	private static final String TABLE_VALUES = "values666";
-	
+
 	private static final String KEY_ID = "id";
 	private static final String NAME_DATE = "date";
 	private static final String NAME_TEMP = "temp";
 	private static final String NAME_CTEMP = "rgbtemp";
 	private static final String NAME_LINT = "intensity";
-	
-	private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_VALUES;
-	
-	
+
+	private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+			+ TABLE_VALUES;
+
+	// counts the amount of columns in the table
+	private int count = 0;
+
 	public DatabaseHandler(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-	
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
 	@Override
-	public void onCreate(SQLiteDatabase db){
+	public void onCreate(SQLiteDatabase db) {
 		String CREATE_VALUES_TABLE = "CREATE TABLE " + TABLE_VALUES + "("
-				+ KEY_ID + " INTEGER PRIMARY KEY," + NAME_DATE + " TEXT," 
-				+ NAME_TEMP + " TEXT," + NAME_CTEMP + " TEXT," 
-				+ NAME_LINT + " TEXT" + ")";
-		
+				+ KEY_ID + " INTEGER PRIMARY KEY," + NAME_DATE + " TEXT,"
+				+ NAME_TEMP + " TEXT," + NAME_CTEMP + " TEXT," + NAME_LINT
+				+ " TEXT" + ")";
+
 		db.execSQL(CREATE_VALUES_TABLE);
 	}
-    @Override
-	
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-		// This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES );
-        onCreate(db);
-    }
-    @Override
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+		// This database is only a cache for online data, so its upgrade policy
+		// is
+		// to simply to discard the data and start over
+		db.execSQL(SQL_DELETE_ENTRIES);
+		onCreate(db);
+	}
+
+	@Override
 	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        onUpgrade(db, oldVersion, newVersion);
-    }
-    
-    public void saveReadings(String date, String temp, String rgbtemp, String intensity) {
-    	SQLiteDatabase db = this.getWritableDatabase();
-    	
+		onUpgrade(db, oldVersion, newVersion);
+	}
+
+	// saves the current sensor info into the table
+	public void saveReadings(String date, String temp, String rgbtemp,
+			String intensity) {
+		SQLiteDatabase db = this.getWritableDatabase();
+
 		ContentValues values = new ContentValues();
 		values.put(NAME_DATE, date);
 		values.put(NAME_TEMP, temp);
 		values.put(NAME_CTEMP, rgbtemp);
 		values.put(NAME_LINT, intensity);
-		
+
 		db.insert(TABLE_VALUES, null, values);
+		count++;
 		db.close();
-    }
+	}
+
+	// returns the ID of last column in the table. UNRELIABLE in the case of deleting/updating stuff
+	public int getIndexOfLast() {
+		return count;
+	}
+
+	//returns wanted reading from the table as an object
+	public Reading getReading(int id) {
+		SQLiteDatabase db = this.getReadableDatabase();
+
+		Cursor cursor = db.query(TABLE_VALUES, new String[] { KEY_ID,
+				NAME_DATE, NAME_TEMP, NAME_CTEMP, NAME_LINT }, KEY_ID + "=?",
+				new String[] { String.valueOf(id) }, null, null, null, null);
+		if (cursor != null) {
+			cursor.moveToFirst();
+		}
+		Reading reading = new Reading(Integer.parseInt(cursor.getString(0)),
+				cursor.getString(1), cursor.getString(2), cursor.getString(3),
+				cursor.getString(4));
+
+		return reading;
+
+	}
 }
---
+++ src/com/momi/napapp/Reading.java
@@ -0,0 +1,56 @@
+package com.momi.napapp;
+
+public class Reading {
+
+	int _id;
+	String _date;
+	String _temp;
+	String _ctemp;
+	String _intensity;
+
+	public Reading() {
+	};
+
+	public Reading(int id, String date, String temp, String ctemp, String intensity){
+		this._id = id;
+		this._date = date;
+		this._temp = temp;
+		this._ctemp = ctemp;
+		this._intensity = intensity;
+	};
+	
+	public Reading(String date, String temp, String ctemp, String intensity){
+		this._date = date;
+		this._temp = temp;
+		this._ctemp = ctemp;
+		this._intensity = intensity;
+	};
+	
+	public int getID(){
+		return this._id;
+	};
+	
+	public String getTemp(){
+		return this._temp;
+	};
+	
+	public String getCTemp(){
+		return this._ctemp;
+	};
+	
+	public String getIntensity(){
+		return this._intensity;
+	};
+	
+	public void setTemp(String temp){
+		this._temp = temp;
+	};
+	
+	public void setCTemp(String ctemp){
+		this._ctemp = ctemp;
+	};
+	
+	public void setIntensity(String intensity){
+		this._intensity = intensity;
+	};
+}
