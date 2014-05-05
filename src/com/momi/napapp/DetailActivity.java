f6efb5ff6659a16fc7ef5f03a23db088ce2ea7c8
mukesh <pace.akash@gmail.com>
2014-05-05 16:16:12 +0300

    added new detail activity

---
+++ src/com/momi/napapp/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.momi.napapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+	}
+
+}
--- src/com/momi/napapp/MainActivity.java
+++ src/com/momi/napapp/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
+
 import java.util.Calendar;
+
 
 
 
@@ -238,6 +241,12 @@ public class MainActivity extends Activity {
 		});
 	}
 
+	public void gotoActivity(View v) {
+		
+		Intent intent = new Intent(this,DetailActivity.class);
+		startActivity(intent);
+		
+	}
 //	 @Override
 //	 protected void onStart() {
 //	 super.onStart();
